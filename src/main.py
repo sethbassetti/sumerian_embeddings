068b2c7a435822418c3418373e43408872ff0de0
sethbassetti <sethbassetti@gmail.com>
2022-02-26 20:53:31 -0800

    Intermediate changes, added some text in sumerian dataset class

--- src/main.py
+++ src/main.py
@@ -1,31 +1,35 @@
 import os
+import time
 from sumerian_data import SumerianDataset
 from torch.utils.data import DataLoader
 
 def main():
 
     # Constructs the path to the sumerian sentences corpusl
-    data_path = os.path.join("data/sumerian_sentences.txt")
+    data_path = os.path.join("data/sumerian_document_set.atf")
+    vocab_path = "data/sumerian_vocab_list"
 
     # Constructs the dataset and dataloader
-    train_set = SumerianDataset(data_path)
-    train_loader = DataLoader(train_set)
+    train_set = SumerianDataset(data_path, vocab_path)
+
+    # Custom collate function prevents the automatic attempt to tuple dataset into target/label pairs
+    train_loader = DataLoader(train_set, collate_fn=lambda x: x[0])
 
     # Very simple example just to see how data is loaded
     i = 0
-    
     # Iterates through the first 50 example sentences and prints them out
+    start = time.time()
     for example in train_loader:
-        print(example)
+        for token in example:
+            if not train_set.in_vocab(token):
+                print(token)
+                print(example)
+                print(i)
+                exit()
         i += 1
-        if i > 100:
-            break
 
-    with open('data/sumerian_untranslated.atf.txt' ,'r') as infile:
-        for line in infile.readlines():
-            if "lang akk" in line:
-                print(line)
-
+    print(str(time.time() - start))
+    
 
 
 
--- src/sumerian_data.py
+++ src/sumerian_data.py
@@ -1,3 +1,4 @@
+from distutils import text_file
 import torch
 from torch.utils.data import Dataset
 from typing import List
@@ -17,7 +18,12 @@ class SumerianDataset(Dataset):
         self.sentences: List[str] = self.load_corpus(corpus_dir)
 
     def load_vocab(self, vocab_dir: str) -> List[str]:
-        pass
+
+        vocab = {}
+        with open(vocab_dir, 'r') as vocab_file:
+            for word in vocab_file.readlines():
+                vocab[word.strip()] = 0
+        return vocab
 
     def load_corpus(self, corpus_dir: str) -> List[str]:
         """Loads the sentences from the corpus into the dataset class
@@ -34,39 +40,106 @@ class SumerianDataset(Dataset):
 
         with open(corpus_dir, "r") as corpus:
 
-            is_sux = False
-            
+            # Contains information about each document 
             document = []
+
             # Iterates through the dataset, appending documents to the list
             for line in corpus.readlines():
 
                 # This indicates the start of a document
                 if line.startswith(NEW_DOC):
+                   
+                    # If the document isn't empty, processes the document into the raw transliterated text
+                    if document:
+                        processed_document = self.process_document(document)
+                    else:
+                        continue
 
-                    # Processes the document into the raw transliterated text
-                    processed_document = self.process_document(document)
+                    # If the processed document is valid, then add it to the documents list
+                    if processed_document is not None:
+                        documents.append(processed_document)
 
-                    documents.append(processed_document)
+                    # Reset the document to an empty list
                     document = []
-                    is_sux = False
 
-                elif SUM_LANG in line:
-                    is_sux = True
-
-                elif is_sux:
-                    document.append(line)
+                # Otherwise create the document
+                else:
+                    document.append(line.strip())
 
 
 
         return documents
 
     def process_document(self, document):
-        for line in document:
-            language = re.compile("#atf: lang sux").match(line)
-            
+        """Takes the raw document data in ATF format and converts it into a tokenized sequence of sumerian words
+
+        Args:
+            document (List[str]): A list of lines containing document information
+
+        Returns:
+            List[str]: A list of tokens that have been processed or the empty string if the document is invalid
+        """
+
+        doc_text = ""
+        
+        # Performs a regex to retrieve the language of this document
+        match = re.compile('#atf: lang(.*)').match(document[0])
+
+        # If the above regex doesn't match, this is not a regular document
+        if match:
+
+            # Only continue if the language is "sux", which is Sumerian
+            if match.group(1).strip() == 'sux':
+                for line in document:
+
+                    # Make sure that the line is not a comment line
+                    if not line.startswith("#"):
+                        # This regex checks for a sequence of non-space characters, followed by a period then a space
+                        # This matches all text lines in the document
+                        text_line_match = re.compile('\S+\.\s(.*)').match(line)
+                        if text_line_match:
+
+                            # Assigns the sumerian transliteration text found in the line
+                            text = text_line_match.group(1)
+                            doc_text += text + " "
+
+        processed_doc_text = self.process_text(doc_text)
+        processed_doc_text = processed_doc_text.split()
+
+        return processed_doc_text
+
+    def process_text(self, text):
+        
+        orig_text = text
+        text = text.replace("#", "")
+        text = text.replace("[", "")
+        text = text.replace("]", "")
+        text = text.replace("<", "")
+        text = text.replace(">", "")
+        text = text.replace("|", "")
+        text = text.replace("?", "")
+        text = text.replace("!", "")
+        #text = text.replace("_", "")
+        
+        text = re.sub(r'\(\$.*\$\)', '', text)
+        
+        
+        while re.compile(r'\s\.\.\.\s').search(text):
+            text = re.sub(r'\s\.\.\.\s', " ", text)
+        text = re.sub(r'^\.\.\.\s', " ", text)
+        #text = text.replace(" x- ", "")
+
+        if "_" in text:
+            return ""
+
+        if " -ta " in text:
+            breakpoint()
+
+        return text
                 
 
-
+    def in_vocab(self, word):
+        return word in self.vocab
     
     def __len__(self):
         """Returns the length of the dataset"""
